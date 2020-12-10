335a2c97a496ccd63d9f0c45987826227d2f7704
Chell <chell_li@sfu.ca>
2020-12-10 09:01:46 -0800

    added more fragments

--- app/src/main/java/li/xiaoxu/greendeco/ui/home/HomeFragment.java
+++ app/src/main/java/li/xiaoxu/greendeco/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package li.xiaoxu.greendeco.ui.home;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,24 +12,39 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
 import li.xiaoxu.greendeco.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    //Recycler GI Education Cards
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.home, R.drawable.content1};
+    static final int REQUEST_CODE_ENTRY = 0;
+    private static final int RESULT_OK = 1;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int rqCode, int rsCode, Intent data){
+        if(rqCode==REQUEST_CODE_ENTRY){
+            if(rsCode==RESULT_OK){
+                Uri webpage = Uri.parse("https://www.google.com");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW,webpage);
+                startActivity(webIntent);
+            }
+        }
+        super.onActivityResult(rqCode, rsCode, data);
     }
 }
