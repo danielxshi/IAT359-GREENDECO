0ead7b70397eb0e14dc72a0333b9a880c8fcdbb0
Chell <chell_li@sfu.ca>
2020-12-10 09:08:01 -0800

    cleaned up and edited nav structure

--- app/src/main/java/li/xiaoxu/greendeco/MapsMarkerActivity.java
+++ app/src/main/java/li/xiaoxu/greendeco/MapsMarkerActivity.java
@@ -39,7 +39,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static li.xiaoxu.greendeco.MainActivity.REQUEST_CODE_ENTRY;
+//import static li.xiaoxu.greendeco.MainActivity.REQUEST_CODE_ENTRY;
 
 /**
  * An activity that displays a Google map with a marker (pin) to indicate a particular location.
