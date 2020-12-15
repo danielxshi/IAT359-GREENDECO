425309a2b4679088cdf854c663691adfb5e7c2d5
Chell Li <chell_li@sfu.ca>
2020-12-15 03:35:59 -0800

    cleared unused assets/style

--- app/src/main/java/li/xiaoxu/greendeco/MainActivity.java
+++ app/src/main/java/li/xiaoxu/greendeco/MainActivity.java
@@ -1,31 +1,12 @@
 package li.xiaoxu.greendeco;
 
-import android.content.Intent;
-import android.graphics.drawable.GradientDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
-import androidx.navigation.NavHost;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import li.xiaoxu.greendeco.ui.home.HomeFragment;
-import li.xiaoxu.greendeco.ui.maps.MapsFragment;
-import li.xiaoxu.greendeco.ui.settings.SettingsFragment;
-import li.xiaoxu.greendeco.ui.sites.SitesFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
         bottomNav.setOnNavigationItemSelectedListener(item -> {
 
             switch (item.getItemId()) {
-
                 case R.id.navigation_home:
                     navController.navigate(R.id.navigation_home);
+                    navController.popBackStack(R.id.navigation_home, true);
                     return true;
 
                 case R.id.navigation_sites:
