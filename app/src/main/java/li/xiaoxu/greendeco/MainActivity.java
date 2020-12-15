9e657b2316b447b5e05e8af358bdfac4753d5eab
Chell Li <chell_li@sfu.ca>
2020-12-15 05:13:37 -0800

    fixed navigation bar UI not switching bug
    help from https://developer.android.com/guide/navigation/navigation-ui

--- app/src/main/java/li/xiaoxu/greendeco/MainActivity.java
+++ app/src/main/java/li/xiaoxu/greendeco/MainActivity.java
@@ -5,27 +5,27 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //https://www.akshayrana.in/2020/07/bottom-navigation-bar-in-android.html
+//    https://developer.android.com/guide/navigation/navigation-ui
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
 
         bottomNav.setOnNavigationItemSelectedListener(item -> {
-
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     navController.navigate(R.id.navigation_home);
-                    navController.popBackStack(R.id.navigation_home, true);
                     return true;
 
                 case R.id.navigation_sites:
